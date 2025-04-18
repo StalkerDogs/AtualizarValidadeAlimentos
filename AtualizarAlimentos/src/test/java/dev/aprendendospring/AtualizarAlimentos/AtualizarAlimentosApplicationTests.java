45d43f10fcd885ec3877bf45ecffb65b27c46185
Samuel Arruda <arrudasamuel98@gmail.com>
2025-04-18 15:57:08 -0300

    Primeiro upload de arquivo feito.
    Esses arquivos não foram tiveram seus uploads feitos por comandos.

---
+++ AtualizarAlimentos/src/test/java/dev/aprendendospring/AtualizarAlimentos/AtualizarAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aprendendospring.AtualizarAlimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtualizarAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
